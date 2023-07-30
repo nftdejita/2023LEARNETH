d5ce5c195f94076c9ffa75624eed8bca369403c0
Yasuhisa Shirota <nst.shirotays@gmail.com>
2023-07-30 15:17:35 +0900

    check

---
+++ 01_Solidity/18_receiving-ether/receivingEther.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract TestContract {
+    mapping(address => uint) public balances;
+
+    // TODO: Implement receive and fallback functions
+
+    function deposit() public payable {
+        // TODO: Implement this function
+    }
+}
