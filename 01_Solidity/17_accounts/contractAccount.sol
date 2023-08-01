fee36695d548e3d1574110c62d74a0a37f2b45f6
Yasuhisa Shirota <nst.shirotays@gmail.com>
2023-08-01 10:56:57 +0900

    refine

--- 01_Solidity/17_accounts/contractAccount.sol
+++ 01_Solidity/17_accounts/contractAccount.sol
@@ -11,8 +11,9 @@ contract Charity {
     function donate() public payable {}
 
     function withdraw() public {
-        // TODO: Implement owner check
+        require(msg.sender == owner, "Only the owner can withdraw");
         uint amount = address(this).balance;
-        payable(owner).transfer(amount);
+        (bool sent, ) = payable(owner).call{value: amount}("");
+        require(sent, "Failed to send Ether");
     }
 }
